a198dbe6d1475068cf9c0f2742cf2da664847721
veerapat <veerapat.p66@rsu.ac.th>
2025-08-31 17:20:32 +0700

    Enhance packet logging and statistics reporting
    - Updated logging format to include the day of the week in 'log.csv'.
    - Integrated pandas for reading and displaying log data in a structured format.
    - Removed commented-out print statements for cleaner code.
    - Improved readability and organization of the code in the Info class.

--- pktdetector.py
+++ pktdetector.py
@@ -1,19 +1,27 @@
 import pyshark
 import datetime
+import pandas as pd
 capture = pyshark.LiveCapture(interface='en1')
 # Define lists to store IP header fields
 class Statistics():
     @staticmethod
     def Logs(req_port,src_ip,dst_ip,pkt_time):
-        today = datetime.date.today()
+        current_datetime = datetime.datetime.today()
+        day_of_week = current_datetime.strftime('%A')
         with open('log.csv','a')as f:
-            f.write(f"Date: {today}"+","+f"Time: {pkt_time}"+","+f"Source IP: {src_ip}"+","+
-                    f"Destination IP: {dst_ip}"+","+
-                    f"Requested Port: {req_port}"+","+"\n")
-        print("Logging...")
+            f.write(f"{day_of_week}"+","+f"{pkt_time}"+","+f"{src_ip}"+","+
+                    f"{dst_ip}"+","+
+                    f"{req_port}"+"\n")
+        # print("Logging...")
+        Security.RequestStatistics()
 
 class Security():
     syncflooding=False
+    @staticmethod 
+    def RequestStatistics():    
+     read = pd.read_csv('log.csv', names=['Day', 'Time', 'Source IP', 'Destination IP', 'Destination Port'])
+     df = pd.DataFrame(read)
+     print(df)
 
     @staticmethod
     def SyncFlooding():
@@ -51,6 +59,25 @@ class Info:
         try:
             for packet in capture.sniff_continuously():
                 try:
+                   
+
+                        # print(f"[TCP] {source_address}:{source_port} -> {destination_address}:{destination_port}, "
+                        #       f"SEQ={packet.tcp.seq}, ACK={packet.tcp.ack}, LEN={packet.tcp.len}, "
+                        #       f"Flags(SYN={packet.tcp.flags_syn}, ACK={packet.tcp.flags_ack}, FIN={packet.tcp.flags_fin}, RST={packet.tcp.flags_reset}), "
+                        #       f"Time={packet_time}"   
+                    if 'IP' in packet:
+                        ipversion.append(packet.ip.version)
+                        ipheaders_length.append(packet.ip.hdr_len)
+                        iptos.append(packet.ip.dsfield)
+                        ipipidentification.append(packet.ip.id)
+                        ipflags.append(packet.ip.flags)
+                        ipfragment_offset.append(packet.ip.frag_offset)
+                        ipttl.append(packet.ip.ttl)
+                        ipprotocol.append(packet.ip.proto)
+                        ipheader_checksum.append(packet.ip.checksum)
+                        ipsource_address.append(packet.ip.src)
+                        ipdestination_address.append(packet.ip.dst)
+                       
                     if 'TCP' in packet:
                         protocol = packet.transport_layer
                         source_address = packet.ip.src
@@ -72,31 +99,11 @@ class Info:
                         tcp_flag_ack.append(packet.tcp.flags_ack)
                         tcp_flag_fin.append(packet.tcp.flags_fin)
                         tcp_flag_rst.append(packet.tcp.flags_reset)
-
-                        # print(f"[TCP] {source_address}:{source_port} -> {destination_address}:{destination_port}, "
-                        #       f"SEQ={packet.tcp.seq}, ACK={packet.tcp.ack}, LEN={packet.tcp.len}, "
-                        #       f"Flags(SYN={packet.tcp.flags_syn}, ACK={packet.tcp.flags_ack}, FIN={packet.tcp.flags_fin}, RST={packet.tcp.flags_reset}), "
-                        #       f"Time={packet_time}")
-                    elif 'UDP' in packet:
-                        pass
-
-                    if 'IP' in packet:
-                        ipversion.append(packet.ip.version)
-                        ipheaders_length.append(packet.ip.hdr_len)
-                        iptos.append(packet.ip.dsfield)
-                        ipipidentification.append(packet.ip.id)
-                        ipflags.append(packet.ip.flags)
-                        ipfragment_offset.append(packet.ip.frag_offset)
-                        ipttl.append(packet.ip.ttl)
-                        ipprotocol.append(packet.ip.proto)
-                        ipheader_checksum.append(packet.ip.checksum)
-                        ipsource_address.append(packet.ip.src)
-                        ipdestination_address.append(packet.ip.dst)
-
                         Statistics.Logs(destination_port,source_address,destination_address,packet_time)
                 except AttributeError:
                     pass
         except (KeyboardInterrupt, EOFError):
             print("\nStopped packet capturing safely.")
+            return
 
 Info.packetdetector()
