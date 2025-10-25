42ff952cf405b1dd5f72800431bd5e5b612f1a49
Veerapat <veerapat.p66@rsu.ac.th>
2025-10-25 21:03:57 +0700

    Refactor DDoS and port scan detection logic, enhancing IP blocking and adjusting thresholds for improved accuracy

--- pktdetector.py
+++ pktdetector.py
@@ -4,38 +4,93 @@ import pyshark
 import datetime
 import pandas as pd
 import os
+
 capture = pyshark.LiveCapture(interface='en1')
 
 class Statistics():
     @staticmethod
-    def Logs(req_port, src_ip, dst_ip, pkt_time, syn_flag, ack_flag):
-        current_datetime = datetime.datetime.today()
-        day_of_week = current_datetime.strftime('%A')
-        log_file = 'log.csv'
+#     def Logs(req_port, src_ip, dst_ip, pkt_time, syn_flag, ack_flag):
+#         current_datetime = datetime.datetime.today()
+#         day_of_week = current_datetime.strftime('%A')
+#         log_file = 'log.csv'
 
-       
-        if not os.path.exists(log_file):
-            with open(log_file, 'w') as f:
-                f.write("Day,Time,Source IP,Destination IP,Destination Port,SYN Flag,ACK Flag\n")
+#         if not os.path.exists(log_file):
+#             with open(log_file, 'w') as f:
+#                 f.write("Day,Time,Source IP,Destination IP,Destination Port,SYN Flag,ACK Flag\n")
 
-        # เขียนข้อมูลต่อท้าย
-        with open(log_file, 'a') as f:
-            f.write(f"{day_of_week},{pkt_time},{src_ip},{dst_ip},{req_port},{syn_flag},{ack_flag}\n")
+#         with open(log_file, 'a') as f:
+#             f.write(f"{day_of_week},{pkt_time},{src_ip},{dst_ip},{req_port},{syn_flag},{ack_flag}\n")
 
-        # print(f"Logged packet: {src_ip} -> {dst_ip} port {req_port}")
-        Security.PortScanDetect()
+#         Security.PortScanDetect()
+#         Security.DosDetect()
 
-class Security():
+# class Security():
+    
+#     @staticmethod
+#     def DosDetect():
+#         today = datetime.datetime.today()
+#         myipaddress = netifaces.ifaddresses('en1')[netifaces.AF_INET][0]['addr']
+#         df = pd.read_csv('log.csv')
+#         baseline_weekend=df[df['Day'].isin(['Saturday', 'Sunday'])]
+#         baseline_weekday=df[df['Day'].isin(['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday'])]
+
+#         if(today.strftime('%A') in ['Saturday', 'Sunday']):
+#             df_http = df[(df['Destination Port']==80) | (df['Destination Port']==3000)]
+#             byip = df_http.groupby('Source IP')
+#             mean_requests = byip.size().mean()
+#             http_requestsperip = df_http.groupby('Source IP').size()
+#             for ip, request_count in http_requestsperip.items():
+#                 if request_count > mean_requests * 3 and ip != myipaddress:
+#                     blocklist = pd.read_csv('Block.csv')
+#                     if ip not in blocklist['Source IP'].values:
+#                         Iptables.block_ip(ip)
+         
+#             df_syn = df[df['SYN Flag'] == 1]
+#             byip_syn = df_syn.groupby('Source IP')
+#             syn_requestsperip = byip_syn.size()
+#             mean_syn_requests = byip_syn.size().mean()
+#             print("Mean SYN Requests (Weekend):", mean_syn_requests)
+#             for ip, syn_count in syn_requestsperip.items():
+#                 if syn_count > mean_syn_requests * 3 and ip != myipaddress:
+#                     print("SYN Flooding:", ip)
+#                     blocklist = pd.read_csv('Block.csv')
+#                     if ip not in blocklist['Source IP'].values:
+#                         Iptables.block_ip(ip)
+
+#         if(today.strftime('%A') in ['Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday']):
+#             df_http = df[(df['Destination Port']==80) | (df['Destination Port']==3000)]
+#             byip = df_http.groupby('Source IP')
+#             mean_requests = byip.size().mean()
+#             http_requestsperip = df_http.groupby('Source IP').size()
+#             for ip, request_count in http_requestsperip.items():
+#                 if request_count > mean_requests * 2 and ip != myipaddress:
+#                     print("HTTP Flooding:", ip)
+#                     blocklist = pd.read_csv('Block.csv')
+#                     if ip not in blocklist['Source IP'].values:
+#                         Iptables.block_ip(ip)
+            
+#             df_syn = df[df['SYN Flag'] == 1]
+#             byip_syn = df_syn.groupby('Source IP')
+#             syn_requestsperip = byip_syn.size()
+#             mean_syn_requests = byip_syn.size().mean()
+#             for ip, syn_count in syn_requestsperip.items():
+#                 if syn_count > mean_syn_requests * 3 and ip != myipaddress:
+#                     print("SYN Flooding:", ip)
+#                     blocklist = pd.read_csv('Block.csv')
+#                     if ip not in blocklist['Source IP'].values:
+#                         Iptables.block_ip(ip)
+
     @staticmethod
     def PortScanDetect():
+        myipaddress = netifaces.ifaddresses('en1')[netifaces.AF_INET][0]['addr']
         df = pd.read_csv('log.csv')
         df = df[['Source IP', 'Destination Port']]
         byip = df.groupby('Source IP')
         request_ports = byip['Destination Port'].nunique()
-        ## For real use
+        ##  real
         # threshold = 300
-        ##For testing purpose set 15
-        threshold = 1
+        ##For testing  15
+        threshold = 50
 
         log_file = 'Block.csv'
         if not os.path.exists(log_file):
@@ -45,14 +100,10 @@ class Security():
         dfportblock = pd.read_csv(log_file)
 
         for ip, port in request_ports.items():
-            if port > threshold:
-                if ip not in dfportblock['Source IP'].values:
-                    print("Port scanning detected from:", ip)
-                    Iptables.block_ip(ip)
-                    
+            if port > threshold and ip !=myipaddress:
+                print("Port scanning detected from:", ip)
                
 
- ####################################################################               
 class Iptables:
     @staticmethod
     def block_ip(ip_address):
@@ -63,7 +114,7 @@ class Iptables:
         log_file = 'Block.csv'
         if not os.path.exists(log_file):
             with open(log_file, 'w') as f:
-                f.write("Source IP")
+                f.write("Source IP\n")
 
         with open(log_file, 'a') as f:
             f.write(f"{ip_address}\n")
@@ -84,7 +135,6 @@ class Iptables:
                     
                 print(f"Unblocked all rules for IP {ip_address} in chain {chain}")
 
-###########################################################################     
 class Info:
     @staticmethod
     def packetdetector():
@@ -92,25 +142,23 @@ class Info:
         input_mode = input("Enter mode number: ")
         if(input_mode =='1'):
             try:
-             for packet in capture.sniff_continuously():
-                try:
-                    if 'TCP' in packet:
-                        source_address = packet.ip.src
-                        source_port = packet.tcp.srcport
-                        destination_address = packet.ip.dst
-                        destination_port = packet.tcp.dstport
-                        packet_time = packet.sniff_time
+                for packet in capture.sniff_continuously():
+                    try:
+                        if 'TCP' in packet:
+                            source_address = packet.ip.src
+                            source_port = packet.tcp.srcport
+                            destination_address = packet.ip.dst
+                            destination_port = packet.tcp.dstport
+                            packet_time = packet.sniff_time
 
-                        syn_flag = 1 if str(packet.tcp.flags_syn).lower() == 'true' else 0
-                        ack_flag = 1 if str(packet.tcp.flags_ack).lower() == 'true' else 0
+                            syn_flag = 1 if str(packet.tcp.flags_syn).lower() == 'true' else 0
+                            ack_flag = 1 if str(packet.tcp.flags_ack).lower() == 'true' else 0
 
-                        Statistics.Logs(destination_port, source_address, destination_address, packet_time, syn_flag, ack_flag)
-                except AttributeError:
-                    pass
+                            Statistics.Logs(destination_port, source_address, destination_address, packet_time, syn_flag, ack_flag)
+                    except AttributeError:
+                        pass
             except (KeyboardInterrupt, EOFError):
-             print("Service Has been terminated")
-             return
-
-        
+                print("Service Has been terminated")
+                return
 
 Info.packetdetector()
